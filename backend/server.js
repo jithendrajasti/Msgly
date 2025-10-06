6f4192ce244c3106cb069cdebe8868ac22840b39
jithendrajasti <jithendrajasti11@gmail.com>
2025-10-06 18:57:31 +0530

    syntax corrections

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ app.get('/', (req, res) => {
     res.send("Hello jithu!");
 });
 
-const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
